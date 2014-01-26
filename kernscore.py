8f1bbc035e713d56ee3d0b4406b10c6478a3329c
Yuri Broze <yuribroze@gmail.com>
2014-01-26 18:34:49 -0500

    Encapsulated KernScore object.

--- kernscore.py
+++ kernscore.py
@@ -10,55 +10,19 @@ pitches_re = re.compile('[a-gA-Gr]+')
 durations_re = re.compile('[0-9.]+')
 modifiers_re = re.compile('[^a-zA-Zr0-9.]')
 
-reference_data = {}
-global_comments = []
-comments = []
-barlines = []
-parts = []
 
-def new_part(declaration):
-    return { 'declaration': declaration,
-             'data': [] }
-
-def new_barline(kern_line, beat):
-    barline = {'beat': beat}
-    first_token = kern_line.split('\t')[0]
-
-    if '==@' in first_token:
-        barline = { 'type': 'final',
-                    'number': None }
-    elif '==' in first_token:
-        barline = { 'type': 'double',
-                    'number': first_token[2:] }
-    elif '=' in first_token:
-        barline = { 'type': 'single',
-                    'number': first_token[1:] }
-
-    return barline
-
-def new_token(token_string, beat):
-    """Create a new token dictionary from a kern
-       token string.
-    """
-    if token_string == '.':
-        token = {}
-    else:
-        pitch = ''.join(pitches_re.findall(token_string))
-        duration = ''.join(durations_re.findall(token_string))
-        modifiers = ''.join(modifiers_re.findall(token_string))
-
-        token = { 'pitch': pitch,
-                  'duration': float(duration),
-                  'modifiers': modifiers,
-                  'beat': beat }
-
-    return token
-
-def read_kernpath(path):
+class KernScore:
     """Only verified for Bach Chorales right now.
     """
-    current_beat = 0
-    with open(path) as kernfile:
+    reference_data = {}
+    global_comments = []
+    comments = []
+    barlines = []
+    parts = []
+
+    def __init__(self, path):
+        current_beat = 0
+        kernfile = open(path)
 
         for line in kernfile:
             line = line.strip()
@@ -69,10 +33,11 @@ def read_kernpath(path):
                 reference_data[refkey] = line[8:]
 
             elif line[:2] == '!!':
-                global_comments.append(line[4:])
+                comments.append(line[4:])
 
             elif '!' in line:
-                comments[current_beat].append(line)
+                # Discard inline comments.
+                pass
 
             # Parse interpretations.
             elif '**kern' in line:
@@ -120,8 +85,46 @@ def read_kernpath(path):
 
                 current_beat += max( t.get('duration', 0)
                                      for t in tokens ) ** -1
-                
-    # Initial import is complete. Now begin manipulating the
-    # imported data into its preferred form.
+
+        # Close the file.
+        kernfile.close()
 
 
+# Sub-parsers / models.
+def new_part(declaration):
+    return { 'declaration': declaration,
+             'data': [] }
+
+def new_barline(kern_line, beat):
+    barline = {'beat': beat}
+    first_token = kern_line.split('\t')[0]
+
+    if '==@' in first_token:
+        barline = { 'type': 'final',
+                    'number': None }
+    elif '==' in first_token:
+        barline = { 'type': 'double',
+                    'number': first_token[2:] }
+    elif '=' in first_token:
+        barline = { 'type': 'single',
+                    'number': first_token[1:] }
+
+    return barline
+
+def new_token(token_string, beat):
+    """Create a new token dictionary from a kern
+       token string.
+    """
+    if token_string == '.':
+        token = {}
+    else:
+        pitch = ''.join(pitches_re.findall(token_string))
+        duration = ''.join(durations_re.findall(token_string))
+        modifiers = ''.join(modifiers_re.findall(token_string))
+
+        token = { 'pitch': pitch,
+                  'duration': float(duration),
+                  'modifiers': modifiers,
+                  'beat': beat }
+
+    return token
