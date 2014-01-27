b31b8de67f0c21671da94b4683db67ba8b4dc2d1
Yuri Broze <yuribroze@gmail.com>
2014-01-26 19:01:26 -0500

    Added pitch manipulating utility.

---
+++ utils.py
@@ -0,0 +1,19 @@
+"""
+Utilities for working with pitches, durations, etc.
+"""
+
+PC = [0, 2, 4, 5, 7, 9, 11]
+
+def humpitch_to_number(pitch):
+    """Convert a pitch string such as 'AA' to a MIDI note number.
+    """
+    note = ''.join( c for c in pitch if c not in '#-n' )
+
+    if note.isupper():
+        octave = 4 - len(note)
+        scale_degree = ( ord(note[0]) - 67 ) % 7
+    else:
+        octave = 3 + len(note)
+        scale_degree = ( ord(note[0]) - 99 ) % 7
+
+    return 12 * octave + PC[scale_degree] + note.count('#') - note.count('-')
