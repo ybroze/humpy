535a0a22b908b234d3b5a0ef46704da335172f4a
Yuri Broze <yuribroze@gmail.com>
2014-01-26 20:26:04 -0500

    Namechange and correct octave calculation.

--- utils.py
+++ utils.py
@@ -4,14 +4,14 @@ Utilities for working with pitches, durations, etc.
 
 PC = [0, 2, 4, 5, 7, 9, 11]
 
-def pitch_to_number(pitch):
+def pitch_to_midinote(pitch):
     """Convert a humdrum pitch string such as 'AA#'
        to a MIDI note number.
     """
     note = ''.join( c for c in pitch if c not in '#-n' )
 
     if note.isupper():
-        octave = 4 - len(note)
+        octave = 5 - len(note)
         scale_degree = ( ord(note[0]) - 67 ) % 7
     else:
         octave = 3 + len(note)
