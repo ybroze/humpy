ff4d3158f8f6f54ed888968378441379835be979
Yuri Broze <yuribroze@gmail.com>
2018-02-13 19:27:48 -0500

    Some formatting in kernscore.py.

--- kernscore.py
+++ kernscore.py
@@ -8,9 +8,9 @@ from midiutil.MidiFile import MIDIFile
 
 from humpy.utils import pitch_to_midinote, recip_to_duration
 
-pitches_re = re.compile('[ra-gA-Gn#\-]+')
-recip_re = re.compile('[0-9.]+')
-modifiers_re = re.compile('[^ra-gA-Gn#\-0-9.]')
+PITCHES_RE = re.compile('[ra-gA-Gn#\-]+')
+RECIP_RE = re.compile('[0-9.]+')
+MODIFIERS_RE = re.compile('[^ra-gA-Gn#\-0-9.]')
 
 
 class KernScore:
@@ -45,8 +45,7 @@ class KernScore:
         return cadences
 
     def import_kernfile(self, file_path):
-        """Import a kernfile and overwrite the internal
-           state of the KernScore.
+        """Import a kernfile and overwrite the internal state of the KernScore.
         """
         if self.file_path:
             self.__init__()
@@ -164,31 +163,33 @@ def new_barline(kern_line, beat):
 
     return barline
 
+
 def new_section(kern_line, beat):
     """Make a new section dict.
     """
     first_token = kern_line.split('\t')[0]
 
-    return { 'beat': beat,
-             'section': first_token[2:] }
+    return {
+        'beat': beat,
+        'section': first_token[2:],
+    }
+
 
 def new_token(token_string, beat, timebase=4):
-    """Create a new token dictionary from a kern
-       token string.
+    """Create a new token dictionary from a kern token string.
 
     @param token_string: A single humdrum token.
     @param beat: The beat the token falls on.
     @param timebase: The recip indication of the beat note.
     @return: a token dict.
-
     """
     if token_string[0] == '.':
         token = {}
 
     else:
-        pitch = ''.join(pitches_re.findall(token_string))
-        modifiers = ''.join(modifiers_re.findall(token_string))
-        recip = ''.join(recip_re.findall(token_string))
+        pitch = ''.join(PITCHES_RE.findall(token_string))
+        modifiers = ''.join(MODIFIERS_RE.findall(token_string))
+        recip = ''.join(RECIP_RE.findall(token_string))
 
         token = {
             'pitch': pitch,
@@ -201,11 +202,13 @@ def new_token(token_string, beat, timebase=4):
 
     return token
 
+
 def new_cadence(partstack):
     """Create a new cadence object using a partstack.
        TODO: figure out what really should be represented.
     """
-    return {'pitches': tuple( p['pitch'] for p in partstack ),
-            'midinotes': tuple( p['midinote'] for p in partstack ),
-            'beats': tuple( p['beat'] for p in partstack ),
+    return {
+        'pitches': tuple( p['pitch'] for p in partstack ),
+        'midinotes': tuple( p['midinote'] for p in partstack ),
+        'beats': tuple( p['beat'] for p in partstack ),
     }
